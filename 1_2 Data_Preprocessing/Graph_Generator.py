300992c52370be932c7df6ded0932e296fda4826
ToniNabrotzky <toninabrotzky@gmail.com>
2025-03-10 17:10:53 +0100

    Update Graph_Generator.py
    added main function as serialized graph generation,
    added generate graph from_json for individual graph generation,
    added try on plot_graph_3d_from json but nodes are duplicating when adding edges

--- 1_2 Data_Preprocessing/Graph_Generator.py
+++ 1_2 Data_Preprocessing/Graph_Generator.py
@@ -1,11 +1,89 @@
+import os
 import json
 import networkx as nx
 import matplotlib.pyplot as plt
+
 from mpl_toolkits.mplot3d import Axes3D
+from pathlib import Path
+
 
 """Hauptfunktion"""
+def main():
+    """Hauptfunktion zum Erstellen des Graphen und der Kantenlabels"""
+    global script_dir
+    script_dir = Path(__file__).parent
+    # script_dir = Path.cwd() # Alternative: Path(__file__).parent
+    # print(f"Script-Dir - {script_dir.exists()}: {script_dir}") #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    # json_folder_names = ["Modell_2_Parametrisch IfcOpenShell", "Modell_2_Tutorial BimVision"]
+    json_folder_names = ["Modell_2_DataBase", "Modell_2_Parametrisch Stahlbeton"]
+    json_folder_names = []
+    json_folder_paths = get_folder_paths(script_dir, json_folder_names)
+
+    for json_folder_path in json_folder_paths:
+        # print(f"json-Folder-Path - {json_folder_path.exists()}:  {json_folder_path}") #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        generate_graph_from_json(json_folder_path)
+    
+    ##Teste einzelne Graphen
+    json_file = r"H:\1_2 Data_Preprocessing\Modell_2_Parametrisch Stahlbeton\JSON_Graph\Parametrisches_Modell Index_016.json"
+    plot_graph_3D_from_json(json_file)
+
+
+def generate_graph_from_json(json_folder_path):
+    """Extrahiert Daten aus JSON-Dateien und speichert sie in einem Graphen"""
+    ## Durchsuche folder_path nach allen JSON-Dateien
+    if not json_folder_path.exists():
+        print(f"__Ordner nicht gefunden. Fehlender Pfad: {json_folder_path}")
+    else:
+        print(f"__Ordner gefunden. Suche JSON-Dateien in: {json_folder_path}")
+        json_files = [datei for datei in os.listdir(json_folder_path) if datei.lower().endswith('.json')]
+        # print(f"JSON-Dateien: {json_files}") #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+        if not json_files:
+            print(f"  __Keine JSON-Dateien gefunden. Extraktion abgebrochen.")
+        else:
+            print(f"  __JSON-Dateien gefunden. Starte Extraktion...")
+
+            for json_file in json_files:
+                # print('json_file (generate_graph_from_json): ', json_file) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                print(f"\n  __Analysiere Datei: {json_file}")
+
+                ## Öffne JSON-Datei
+                json_file_path = json_folder_path / json_file
+
+                ## Extrahiere Elemente
+                elements = get_elements_from_json(json_file_path)
+
+                ## Graph erstellen
+                G = create_graph_from_elements(elements)
+                print("Graph unverbunden: ", G) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                # plot_graph_3D(G)
+
+                ## Kanten erstellen
+                edges = create_edges_from_elements(elements)
+                print("Kanten: ", edges) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                ## Kanten dem Graphen hinzufügen
+                G.add_edges_from(edges)
+                print("Graph mit Kanten: ", G) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                # plot_graph_3D(G)
+
+                ## Speicher Graph und Kantenliste
+                save_graph_to_json(G, json_file_path)
+
+
+
+"""Hilfsfunktionen """
+def get_folder_paths(script_dir, folder_names):
+    """Gibt eine Liste von Pfaden zu den Ordnern zurück"""  
+    json_folder_paths = [script_dir / folder_name / 'JSON_IFC' for folder_name in folder_names]
+    # # print(f"Folder-Paths: {json_folder_paths}") #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    return json_folder_paths
+
+
 def get_elements_from_json(json_file):
     """Extrahiert Daten aus JSON-Datei"""
+    # print('json_file (get_elements_from_json): ', json_file) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     with open(json_file, "r") as f:
         data = json.load(f)
         elements = {}
@@ -14,7 +92,7 @@ def get_elements_from_json(json_file):
             error_info = element.get('obj_properties', {}).get('Pset_ErrorInfo', {})
             # print('error_info: ', error_info) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             attr = {
-                # General attributes
+                # general attributes
                 'id': element['id'],
                 'ifc_class': element['ifc_type'],
                 'name': element['name'],
@@ -132,8 +210,6 @@ def create_edges_from_elements(elements):
     return edges
 
 
-
-"""Hilfsfunktionen"""
 def plot_graph_2D(G):
     """Plottet den 2D-Graphen mit den Knotenpositionen"""
     pos = {node: data['position'] for node, data in G.nodes(data= True)}
@@ -170,14 +246,73 @@ def plot_graph_3D(G):
         x= [pos[edge[0]][0], pos[edge[1]][0]]
         y= [pos[edge[0]][1], pos[edge[1]][1]]
         z= [pos[edge[0]][2], pos[edge[1]][2]]
-        ax.plot(x, y, z, color= 'black')
+        ax.plot(x, y, z, color= 'black')    
     
     plt.show()
 
 
-## Modellpfade
-Param_Modell_001 = r"H:\1_2 Data_Preprocessing\Modell_2_Parametrisch IfcOpenShell\Parametrisches_Modell Index_001.json"
-CoreHouse = r"H:\1_2 Data_Preprocessing\Modell_2_Parametrisch IfcOpenShell\CoreHouse_VanDerRohe.json"
+def plot_graph_3D_from_json(json_file_path):
+    """Plottet den 3D-Graphen mit den Knotenpositionen aus einer JSON-Datei"""
+    with open(json_file_path, "r") as f:
+        graph_data = json.load(f)
+        # print(len(graph_data))
+    # nodes = graph_data['nodes']
+    # print(len(nodes))
+    # print('nodes: ', nodes)
+    # for idx, node in enumerate(nodes):
+    #     print('idx und node: ', idx, node) # sind beide das gleiche (0 0 bis 78 78)
+        # print('node_pos: ', node['position'])
+    # print('graph_data: ', graph_data['nodes'])
+
+    
+    G = nx.Graph()
+    for node, data in graph_data['nodes'].items():
+        # print('graph_node: ', node)
+        # print('graph_data: ', data)
+        # print('graph_data_pos: ', data['position'])
+        G.add_node(node, **data)
+        # print(G)
+    # print(G)
+    
+    # pos = {node: data['position'] for node, data in G.nodes(data= True)}
+    # print('pos: ', pos)
+    print('g before add egdes', G)
+    edges = [(edge[0], edge[1]) for edge in graph_data['edges']]
+    edges = [(0,1)]
+    edges = [(0,1), (1,2), (2,3)]
+    print('graph_data_edges: ', graph_data['edges'])
+    print('edges: ', edges)
+    # G.add_edges_from(graph_data['edges'])
+    G.add_edges_from(edges)
+    print('g after add egdes', G)
+    print("Graph mit Kanten: ", G) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    # plot_graph_3D(G)
+
+
+def save_graph_to_json(G, json_file_path):
+    """Speichert den Graphen und die Kantenliste in einer JSON-Datei"""
+    graph_data = {
+        'nodes': {node: data for node, data in G.nodes(data= True)},
+        'edges': list(G.edges())
+    }
+
+    ## Definiere Namen und Pfad für JSON-Datei
+    graph_folder_path = Path(json_file_path).parent.parent / "JSON_Graph"
+    os.makedirs(graph_folder_path, exist_ok=True) # Ordner erstellen, falls noch nicht vorhanden
+    graph_file_name = Path(json_file_path).name
+    graph_file_path = graph_folder_path / graph_file_name
+    # print('graph_file_path :', graph_file_path) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    ## Speichere Graph und Kantenliste in JSON-Datei
+    with open(graph_file_path, "w") as f:
+        json.dump(graph_data, f, indent= 4)
+    print(f"  __Graph und Kantenliste gespeichert in: {graph_file_path}")
+
+
+## Modellpfade (nicht mehr gebraucht)
+Param_Modell_001 = r"H:\1_2 Data_Preprocessing\Modell_2_Parametrisch IfcOpenShell\JSON\Parametrisches_Modell Index_001.json"
+CoreHouse = r"H:\1_2 Data_Preprocessing\Modell_2_Parametrisch IfcOpenShell\JSON\CoreHouse_VanDerRohe.json"
 BimVision_47L = r"H:\1_2 Data_Preprocessing\Modell_2_Tutorial BimVision\47L.json"
 
 
@@ -185,21 +320,8 @@ BimVision_47L = r"H:\1_2 Data_Preprocessing\Modell_2_Tutorial BimVision\47L.json
 
 
 if __name__ == "__main__":
+    print("___Intitialisiere Skript___")
 
-    json_file = Param_Modell_001 
-    elements = get_elements_from_json(json_file)
+    main()
 
-    ## Graph erstellen
-    G = create_graph_from_elements(elements)
-    # G = create_graph_from_json(json_file)
-    print("Graph leer: ", G) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # plot_graph_3D(G)
-
-    ## Kanten erstellen
-    edges = create_edges_from_elements(elements)
-    # print("Kanten: ", edges) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    ## Kanten dem Graphen hinzufügen
-    G.add_edges_from(edges)
-    print("Graph mit Kanten: ", G) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    plot_graph_3D(G)
+    print("___Skript erfolgreich beendet___")
